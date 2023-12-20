b10d865226088789ae920be5c3d974c7192718cd
Makb <66195638+Elpazzu@users.noreply.github.com>
2023-12-20 10:20:01 +0100

    Update example_3.py

--- Deep-Learning/SL/ISLES-augmentation/example_3.py
+++ Deep-Learning/SL/ISLES-augmentation/example_3.py
@@ -97,3 +97,26 @@ class AugmentatorISLES(object):
                 f.close()
         else:
             print "Data already augmented"
+
+class PreprocessorISLES(object):
+    """Class for ISLES 2017 data preprocessing."""
+
+    """
+        Methods:
+            get_normalization_parameters: creates a dictionary
+                of parameters for volumes' normalization
+            get_alignment_parameters: creates a dictionary
+                of parameters for volumes' alignment
+            name: reproduce PreprocessorISLES object's name
+    """
+    def get_normalization_parameters(self):
+        """Get dictionary of normalization parameters per scan."""
+        raise NotImplementedError()
+
+    def get_alignment_parameters(self):
+        """Get dictionary of alignment parameters per scan."""
+        raise NotImplementedError()
+
+    def name(self):
+        """Class name."""
+        raise NotImplementedError()
